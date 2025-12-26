6f5da822eb4787160b23e675218e1c4c035a5e78
ny4i <toms@bettersoftwaresolutions.com>
2025-12-25 23:01:15 -0500

    Bump version to 2.80.1

--- src/core/Constants.h
+++ src/core/Constants.h
@@ -7,7 +7,7 @@ namespace TR4QT {
 
 // Application info
 constexpr const char* APP_NAME = "TR4QT";
-constexpr const char* APP_VERSION = "2.80.0";  // Numeric frequency entry shortcut
+constexpr const char* APP_VERSION = "2.80.1";  // Fix numeric frequency entry handler
 constexpr const char* APP_ORG = "TR4QT";
 
 // Country file
